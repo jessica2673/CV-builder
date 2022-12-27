0f245cc10e8c33127db2a29d8c899a80299a05ba
Jessica <jessica2003mail@gmail.com>
2022-12-27 14:47:22 -0500

    Implemented a working edit button for General.js

--- src/components/General.js
+++ src/components/General.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import GeneralEdit from './GeneralEdit';
+//import GeneralEdit from './GeneralEdit';
 
 class General extends React.Component {
     constructor(props) {
@@ -11,19 +11,62 @@ class General extends React.Component {
             email: 'abc@email.com',
             phone: '1112223333',
             website: 'helloworld.com',
+            editable: 0 //represents view mode
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    edit = () => {
+        this.setState({editable: 1});
+    }
+
+    handleChange = (e) => {
+        this.setState ({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({editable: 0});
     }
 
     render() {
         const { firstName, lastName, email, phone, website } = this.state;
-        return (
-            <div>
-                <h1>{firstName} {lastName}</h1>
-                <p>{email}</p>
-                <p>{phone}</p>
-                <p>{website}</p>
-            </div>
-        );
+
+        if(this.state.editable === 0) {
+            return (
+                <div>
+                    <h1>{firstName} {lastName}</h1>
+                    <p>{email}</p>
+                    <p>{phone}</p>
+                    <p>{website}</p>
+                    <button onClick={this.edit}>Edit</button>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="editable">First Name: </label>
+                        <input type="text" id="firstName" value={this.state.firstName} onChange={this.handleChange} />
+
+                        <label htmlFor="editable">Last Name: </label>
+                        <input type="text" id="lastName" value={this.state.lastName} onChange={this.handleChange} />
+
+                        <label htmlFor="editable">Email: </label>
+                        <input type="text" id="email" value={this.state.email} onChange={this.handleChange} />
+
+                        <label htmlFor="editable">Phone </label>
+                        <input type="text" id="phone" value={this.state.phone} onChange={this.handleChange} />
+
+                        <label htmlFor="editable">Website: </label>
+                        <input type="text" id="website" value={this.state.website} onChange={this.handleChange} />
+                        <input type="submit" value="Save" />
+                    </form>
+                </div>
+            )
+        }
     }
 }
 
